65f28b79cf556cfc3b801b22a743c333c719bbc5
mohammed-ma1605295 <ma1605295@qu.edu.qa>
2022-10-18 15:28:23 +0300

    test

--- ScaffoldingSessions/ScaffoldSession4/StarterCode/app/src/main/java/com/example/projecttracker/data/repository/TodoListRepo.kt
+++ ScaffoldingSessions/ScaffoldSession4/StarterCode/app/src/main/java/com/example/projecttracker/data/repository/TodoListRepo.kt
@@ -2,23 +2,45 @@ package com.example.projecttracker.data.repository
 
 import com.example.projecttracker.data.entity.Project
 import com.example.projecttracker.data.entity.Todo
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 
 
 object TodoListRepo {
 
-
+    val db by lazy{ Firebase.firestore}
+    val projectDocumentRef by lazy{db.collection("projects")}
+    val todoDocumentRef by lazy{db.collection("todos")}
     //TODO () ADD THE DOCUMENT REFERENCE
 
-    suspend fun getProjects(): List<Project> = TODO()
+    suspend fun getProjects(): List<Project> = projectDocumentRef
+        .get()
+        .await()
+        .toObjects(Project::class.java)
+
     suspend fun addProject(project: Project) {
-        TODO()
+        val documentId = projectDocumentRef.document().id
+        project.id = documentId
+        projectDocumentRef.document(documentId).set(project)
     }
 
-    suspend fun updateProject(updatedProject: Project) = TODO()
-    suspend fun deleteProject(project: Project) = TODO()
-    suspend fun getTodoListByProject(pid: String) = TODO()
-    suspend fun addTodo(todo: Todo) = TODO()
-    suspend fun deleteTodo(id: String) = TODO()
-    suspend fun getTodo(id: String) = TODO()
+    suspend fun updateProject(updatedProject: Project) {
+        projectDocumentRef.document(updatedProject.id).set(updatedProject)
+    }
+    suspend fun deleteProject(project: Project) {
+        projectDocumentRef.document(project.id).delete()
+    }
+    suspend fun getTodoListByProject(projectId: String) = todoDocumentRef
+        .whereEqualTo("projectId",projectId)
+        .get().await().toObjects(Todo::class.java)
+
+    suspend fun addTodo(todo: Todo) {
+        val documentId = todoDocumentRef.document().id
+        todo.id = documentId
+        todoDocumentRef.document(documentId).set(todo)
+    }
+    suspend fun deleteTodo(id: String) = todoDocumentRef.document(id).delete()
+    suspend fun getTodo(id: String) = todoDocumentRef.document(id).get().await().toObject(Todo::class.java)
 }
 
